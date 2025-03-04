52b8f7d08f32bb9df544d9f35268b1e5488e25d5
mikey <mcross1@uropartners.com>
2025-03-04 15:30:23 -0600

    added dice

--- game.go
+++ game.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math/rand"
 )
 
-func Dice() {
-	f := strings.Fields()
-	fmt.Scanln(strings.Fields)
+func Roll(call string, betAmount int) string {
+	var dice1 = []int{1, 2, 3, 4, 5, 6}
+	var dice2 = []int{1, 2, 3, 4, 5, 6}
+	n := rand.Intn(6)
+	i := rand.Intn(6)
+	result := dice1[n] + dice2[i]
+
+	fmt.Println("Rolling dice...You have rolled a ", dice1[n], "\nRolling dice...You have rolled a ", dice2[i])
+	fmt.Println("Your dice total is", result)
+	if call == "under" && result <= 6 {
+		fmt.Printf("You have won! Your payout is %d!", betAmount)
+	} else if call == "under" && result >= 7 {
+		fmt.Printf("You have have lost! Your loss is %d", betAmount)
+	} else if call == "over" && result >= 8 {
+		fmt.Printf("You have won! Your payout is %d!", betAmount)
+	} else if call == "over" && result <= 7 {
+		fmt.Printf("You have have lost! Your loss is %d", betAmount)
+	}
+	return "You lose"
 
 }
--- main.go
+++ main.go
@@ -1,9 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Welcome to over under dice game!")
 	fmt.Println("To place a bet, type out in this format into the channel:\n!bet name amount call (over, under or 7)")
+	fmt.Println("Wins on over or pay out double your bet. Wins on 7 payout x4 your bet")
 	fmt.Println("Example: !bet mikey 500 under")
+
+	// valid bet choices
+	validCalls := map[string]bool{"over": true, "under": true, "7": true}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		// asks for input
+		fmt.Println("\nEnter your bet: (!bet name amount call) ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		// splits the bet input into 4 words
+		parts := strings.Fields(input)
+
+		if len(parts) != 4 || parts[0] != "!bet" {
+			fmt.Println("ah ah ah, invalid command. Command must be in format of: !bet name amount call (over, under or 7)")
+			continue
+		}
+
+		// extract the input values
+		name := parts[1]                  // name field
+		amountStr := parts[2]             //bet amount, will convert to int later
+		call := strings.ToLower(parts[3]) // convert to lowercase
+
+		betAmount, err := strconv.Atoi(amountStr)
+		if err != nil || betAmount <= 0 {
+			fmt.Println("ah ah ah, invalid bet amount. Must be a number over 0")
+			continue
+		}
+
+		// validate the call type (over, under or 7)
+
+		if !validCalls[call] {
+			fmt.Println("ah ah ah, you didnt use the magic word for your call. Use over under or 7")
+			continue
+		}
+
+		// if all checks pass print the bet with the amount as an int
+		fmt.Printf("Bet place!: %s bets %d on %s! Good luck!\n", name, betAmount, call)
+		Roll(call, betAmount)
+		break
+	}
+
 }
